0b90a5ff1636284d98431c13e180a48dd039f3c6
tahminakass2003 <tahminakas8@gmail.com>
2024-06-06 23:53:46 +0600

    first

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Сегодня " + permission(18, 25));
+        System.out.println("Сегодня " + permission(21, 18));
+        System.out.println("Сегодня " + permission(20, 35));
+        System.out.println("Сегодня " + permission(25, 40));
+        System.out.println("Сегодня " + permission(10, 15));
+        int randomAge = generateRandomAge();
+        System.out.println("Сгенерированный рандомный возраст " + randomAge);
     }
+
+    public static String permission(int age, int temperature) {
+        if ((age >= 20 && age <= 45 && temperature >= -20 && temperature <= 30) ||
+                (age < 20 && temperature >= 0 && temperature <= 28) ||
+                (age > 45 || temperature >= -10 && temperature <= 25)) {
+            return "можно идти гулять ";
+        } else return "оставайтесь дома ";
+
+    }
+    public static int generateRandomAge() {
+        Random random = new Random() ;
+        int  minAge = 1;
+        int  maxAge = 100;
+        return random.nextInt(maxAge - minAge );
+    }
+
+
 }
